9dc508c538bb0bf5a8ca420e602d8e3914c38073
weeroshara <weerosharajayasekara@gmail.com>
2020-09-04 10:07:21 +0530

    compleat spring layeard poss using maven

---
+++ src/main/java/lk/ijse/dep/poss/AppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep.poss;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("lk.ijse.dep.poss")
+@Configuration
+public class AppConfig {
+}
---
+++ src/main/java/lk/ijse/dep/poss/AppInitializer.java
@@ -0,0 +1,44 @@
+package lk.ijse.dep.poss;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    private static AnnotationConfigApplicationContext ctx;
+
+    public static AnnotationConfigApplicationContext getApplicationContext(){
+        return ctx;
+    }
+    public static void main(String[] args) {
+        ctx=new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+        launch(args);
+
+
+//        try {
+//            DBConnection.getInstance().getConnection().close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/MainForm.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("JDBC POS");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+    }
+}
